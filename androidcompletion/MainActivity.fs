616dac13308f4f2c43bc7f703549936529e94b6d
nosami <jasonimison@gmail.com>
2017-09-28 16:04:31 +0100

    bug repro

--- androidcompletion/MainActivity.fs
+++ androidcompletion/MainActivity.fs
@@ -3,6 +3,9 @@
 type typeProvider = androidcompletion.Resource
 module x =
     let name = typeProvider.String.app_name
+// No completions here ^
+          
+// but we do get them here  ^  and ^
 
 
 
